16e7c634a0f09bf6da7a4f553a335cf9d52857b2
Sasha-PC\Sasha <aleksandr.v.g.m@gmail.com>
2022-08-09 15:23:02 +0300

    solution Module #6. OOP Fundamentals. Task #1. Class creation

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,24 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String description = "This animal is mostly " + color +
+                ". It has " + numberOfPaws + (numberOfPaws == 1 ? " paw" : " paws") + " and "
+                + (hasFur == true ? "a" : "no") + " fur.";
+        return description;
+    }
+
+    public static void main(String[] args){
+        System.out.println(new Animal("black", 1, false).getDescription());
+    }
 
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,20 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
 
+    public Bird(){
+        super("blue", 2, false);
+    }
+    public String getDescription(){
+        return super.getDescription() + " Moreover, it has 2 wings and can fly.";
+    }
+
+    public static void main(String[] arvs){
+        Animal animal = new Animal("black", 4, true);
+        System.out.println(animal.getDescription());
+        Animal bird = new Bird();
+        System.out.println(bird.getDescription());
+        Animal dog = new Dog();
+        System.out.println(dog.getDescription());
+    }
 }
